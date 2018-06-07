b98dcb0578ef122ef57bf2428ee1ce33133adc2c
hb <hb@aroma-project.org>
2018-06-07 10:14:23 -0700

    Add working overlapScores() for TopDom pair [#2]

--- R/TopDomData.R
+++ R/TopDomData.R
@@ -13,6 +13,7 @@ dim.TopDomData <- function(x) {
   dim(x$counts)
 }
 
+#' @importFrom graphics image
 #' @export
 image.TopDomData <- function(x, transform = log2, ...) {
   image(transform(x$counts), ...)
